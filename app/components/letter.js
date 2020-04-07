a9b0d5cd051da259879f0aa8b2d5386ab9813418
John Wu <johnwu@fb.com>
2020-04-06 22:22:11 -0700

    base letter component

---
+++ app/components/letter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './letter.css';
+
+class Letter extends React.Component {
+  render() {
+    return (
+      <text className="letter">A</text>
+    )
+  }
+}
+
+export default Letter;
