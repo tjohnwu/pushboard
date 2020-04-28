811fc00935acbfa4c77b1435364a0e4e2757ff5d
John Wu <johnwu@fb.com>
2020-04-27 20:07:43 -0700

    Adds base factory components to app

---
+++ app/components/factory.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './factory.css';
+
+// This component is the factory user interface that the user is able to use to
+// manufacture letters.
+class Factory extends React.Component {
+  render() {
+    return (
+      <div className="factory" />
+    )
+  }
+}
+
+export default Factory;
