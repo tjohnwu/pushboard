ae9221f32674a5dba705e8ea6f0835d155ef204b
John Wu <johnwu@fb.com>
2020-04-28 21:24:39 -0700

    Adds key to each letter value

--- app/components/factory.js
+++ app/components/factory.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import './factory.css';
+import React from "react";
+import "./factory.css";
 
 // This component is the factory user interface that the user is able to use to
 // manufacture letters.
@@ -13,7 +13,7 @@ class Factory extends React.Component {
   }
 
   handleAddLetterClick(e) {
-    var value = document.getElementById('letter-input').value;
+    var value = document.getElementById("letter-input").value;
     this.props.addLetter(value);
   }
 
@@ -24,11 +24,24 @@ class Factory extends React.Component {
   render() {
     return (
       <div className="factory">
-        <input id='letter-input' className="letter-input" type="test" maxLength="1"/>
-        <input type="button" value="Add Letter" onClick={this.handleAddLetterClick}/>
-        <input type="button" value="Clear All" onClick={this.handleLetterClear}/>
+        <input
+          id="letter-input"
+          className="letter-input"
+          type="input"
+          maxLength="1"
+        />
+        <input
+          type="button"
+          value="Add Letter"
+          onClick={this.handleAddLetterClick}
+        />
+        <input
+          type="button"
+          value="Clear All"
+          onClick={this.handleLetterClear}
+        />
       </div>
-    )
+    );
   }
 }
 
--- app/components/pushboard.js
+++ app/components/pushboard.js
@@ -1,38 +1,52 @@
-import React from 'react';
-import './pushboard.css';
+import React from "react";
+import "./pushboard.css";
 
-import Letter from './letter.js'
-import Board from './board.js'
-import Factory from './factory.js'
+import Letter from "./letter.js";
+import Board from "./board.js";
+import Factory from "./factory.js";
 
 class Pushboard extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {letters: []};
+    this.state = {
+      letters: {},
+    };
     this.addLetter = this.addLetter.bind(this);
     this.clearLetters = this.clearLetters.bind(this);
+
+    this.getKey = (() => {
+      var counter = 0;
+      return () => counter++;
+    })();
   }
 
   addLetter(letter) {
-    this.setState(state => {
-      return {letters: state.letters.concat(letter)};
+    var key = this.getKey();
+    var additionalLetter = {};
+    additionalLetter[key] = letter;
+    var newState = Object.assign({}, this.state.letters, additionalLetter);
+
+    this.setState((state) => {
+      return { letters: newState };
     });
   }
 
   clearLetters() {
-    this.setState(state => {
-      return {letters: []};
-    })
+    this.setState((state) => {
+      return { letters: [] };
+    });
   }
 
   render() {
     return (
       <div id="pushboard" className="pushboard">
         <Board />
-        <Factory addLetter={this.addLetter} clearLetters={this.clearLetters}/>
-        {this.state.letters.map((key, letter) => <Letter value={letter} />)}
+        <Factory addLetter={this.addLetter} clearLetters={this.clearLetters} />
+        {Object.keys(this.state.letters).map((key) => (
+          <Letter key={key} value={this.state.letters[key]} />
+        ))}
       </div>
-    )
+    );
   }
 }
 
